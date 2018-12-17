3c2b5b4e23833b1bbdc35e469f1ff6d730c5c995
Alex Lebedev <allebedew@gmail.com>
2018-12-17 13:09:11 +0200

    Added mac target

---
+++ OktaAuth macOS/OktaAuth_macOS.h
@@ -0,0 +1,18 @@
+//
+//  OktaAuth_macOS.h
+//  OktaAuth macOS
+//
+//  Created by Alex on 17 Dec 18.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OktaAuth_macOS.
+FOUNDATION_EXPORT double OktaAuth_macOSVersionNumber;
+
+//! Project version string for OktaAuth_macOS.
+FOUNDATION_EXPORT const unsigned char OktaAuth_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OktaAuth_macOS/PublicHeader.h>
+
+
