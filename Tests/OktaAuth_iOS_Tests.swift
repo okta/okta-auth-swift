f60c129e3da395d1c6a352479933dac2b9cbab96
Alex Lebedev <allebedew@gmail.com>
2018-12-17 17:50:16 +0200

    Tests target added

---
+++ Tests/OktaAuth_iOS_Tests.swift
@@ -0,0 +1,32 @@
+//
+//  OktaAuth_iOS_Tests.swift
+//  OktaAuth iOS Tests
+//
+//  Created by Alex on 17 Dec 18.
+//
+
+import XCTest
+
+class OktaAuth_iOS_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
