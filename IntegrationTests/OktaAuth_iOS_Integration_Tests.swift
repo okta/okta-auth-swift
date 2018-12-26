049ef1831117283db59e805523b2c435139e02a4
anastasiiaiurok-okta <anastasiia.iurok@okta.com>
2018-12-26 16:41:33 +0200

    Added Integration tests.

---
+++ IntegrationTests/OktaAuth_iOS_Integration_Tests.swift
@@ -0,0 +1,95 @@
+//
+//  OktaAuth_iOS_Integration_Tests.swift
+//  OktaAuth iOS Integration Tests
+//
+//  Created by Anastasiia Iurok on 12/26/18.
+//
+
+import XCTest
+
+@testable import OktaAuth
+
+class OktaAuth_iOS_Integration_Tests: XCTestCase {
+
+    var domainURL = ProcessInfo.processInfo.environment["DOMAIN_URL"]!
+    var username = ProcessInfo.processInfo.environment["USERNAME"]!
+    var password = ProcessInfo.processInfo.environment["PASSWORD"]!
+
+    var oktaAPI: OktaAPI!
+
+    override func setUp() {
+        oktaAPI = OktaAPI(oktaDomain: URL(string: domainURL)!)
+    }
+
+    override func tearDown() {
+        oktaAPI = nil
+    }
+
+    func testPrimaryAuth_Success() {
+        let exp = expectation(description: "Primary auth request should complete.")
+    
+        oktaAPI.primaryAuthenication(
+            username: username,
+            password: password,
+            audience: nil,
+            relayState: nil,
+            multiOptionalFactorEnroll: false,
+            warnBeforePasswordExpired: false,
+            token: nil, deviceToken: nil)
+        { result in
+            switch result {
+                case .error(let error):
+                    XCTFail("Unexpected error: \(error)")
+                case .success(let response):
+                    XCTAssertEqual("SUCCESS", response.status)
+                    // TODO: extend response verification once model is implemented
+                    break
+            }
+        
+            exp.fulfill()
+        }
+
+        waitForExpectations(timeout: 5.0) { err in
+            if let err = err {
+                XCTFail(err.localizedDescription)
+            }
+        }
+    }
+    
+    func testPrimaryAuth_InvalidPassword() {
+        let exp = expectation(description: "Primary auth request should complete.")
+    
+        oktaAPI.primaryAuthenication(
+            username: username,
+            // generate invalid password
+            password: UUID().uuidString,
+            audience: nil,
+            relayState: nil,
+            multiOptionalFactorEnroll: false,
+            warnBeforePasswordExpired: false,
+            token: nil, deviceToken: nil)
+        { result in
+            switch result {
+                case .error(let error):
+                    guard case let .serverRespondedWithError(oktaError) = error else {
+                        XCTFail("Okta error expected!")
+                        break
+                    }
+                    XCTAssertEqual("E0000004", oktaError.errorCode)
+                    XCTAssertEqual("Authentication failed", oktaError.errorSummary)
+                
+                case .success(_):
+                    XCTFail("Authentication with invalid password should fail!")
+                    break
+            }
+        
+            exp.fulfill()
+        }
+
+        waitForExpectations(timeout: 5.0) { err in
+            if let err = err {
+                XCTFail(err.localizedDescription)
+            }
+        }
+    }
+}
