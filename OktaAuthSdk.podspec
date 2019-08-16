24c4a354dd7681a7adb187e6d54344668dd96606
John Hammerlund <johnhammerlund@gmail.com>
2019-08-16 09:18:36 -0700

    Update podspec

--- OktaAuthSdk.podspec
+++ OktaAuthSdk.podspec
@@ -12,6 +12,9 @@ Integrate your native app with Okta.
   s.source           = { :git => 'https://github.com/okta/okta-auth-swift.git', :tag => s.version.to_s }
 
   s.ios.deployment_target = '10.0'
+  s.watchos.deployment_target = '2.0'
+  s.tvos.deployment_target = '9.0'
+  s.osx.deployment_target = '10.10'
   s.source_files = 'Source/**/*'
   s.swift_version = '4.2'
   s.exclude_files = [
--- Package.swift
+++ Package.swift
@@ -1,23 +1,29 @@
-// swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:4.2
+
+/*
+ * Copyright (c) 2019, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
 
 import PackageDescription
 
 let package = Package(
     name: "OktaAuthNative",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "OktaAuthNative",
             targets: ["OktaAuthNative"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "OktaAuthNative",
             dependencies: []),
