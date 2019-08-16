e14da66ae06158be8521d5e5205165bcb2c96347
John Hammerlund <johnhammerlund@gmail.com>
2019-08-16 08:25:58 -0700

    Adjust SwiftPM package name

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "OktaAuthSwift",
+    name: "OktaAuthNative",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "OktaAuthSwift",
-            targets: ["OktaAuthSwift"]),
+            name: "OktaAuthNative",
+            targets: ["OktaAuthNative"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "OktaAuthSwift",
+            name: "OktaAuthNative",
             dependencies: []),
         .testTarget(
-            name: "OktaAuthSwiftTests",
-            dependencies: ["OktaAuthSwift"]),
+            name: "OktaAuthNativeTests",
+            dependencies: ["OktaAuthNative"]),
     ]
 )
